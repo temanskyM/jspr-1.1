497493ba859c163914a8404e0667f9d8f9cf6f2f
Artiom <temansky@yandex.ru>
2021-05-11 20:41:23 +0300

    Initial commit

---
+++ src/main/java/ru/netology/Main.java
@@ -0,0 +1,17 @@
+package ru.netology;
+
+import java.io.*;
+import java.net.ServerSocket;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.time.LocalDateTime;
+import java.util.List;
+
+public class Main {
+  public static void main(String[] args) {
+    Thread thread = new Thread(new Server());
+    thread.run();
+  }
+}
+
+
