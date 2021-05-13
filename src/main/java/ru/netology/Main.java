e6b59a7d7310e9a35aaf699456ae61be9180ddf5
Artiom <temansky@yandex.ru>
2021-05-13 22:56:46 +0300

    Add ThreadPool

--- src/main/java/ru/netology/Main.java
+++ src/main/java/ru/netology/Main.java
@@ -1,16 +1,9 @@
 package ru.netology;
 
-import java.io.*;
-import java.net.ServerSocket;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.time.LocalDateTime;
-import java.util.List;
-
 public class Main {
   public static void main(String[] args) {
     Thread thread = new Thread(new Server());
-    thread.run();
+    thread.start();
   }
 }
 
