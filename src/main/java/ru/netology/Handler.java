f24aeed7325f3e12e9118f3e9f571cc377bb1cff
Artiom <temansky@yandex.ru>
2021-05-14 19:24:14 +0300

    Add Query

--- src/main/java/ru/netology/Handler.java
+++ src/main/java/ru/netology/Handler.java
@@ -1,21 +1,26 @@
 package ru.netology;
 
+import org.apache.http.NameValuePair;
+import org.apache.http.client.utils.URLEncodedUtils;
+
 import java.io.BufferedOutputStream;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.Socket;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.time.LocalDateTime;
-import java.util.List;
+import java.util.*;
 
 public class Handler implements Runnable {
     final List<String> validPaths = List.of("/index.html", "/spring.svg", "/spring.png", "/resources.html",
             "/styles.css", "/app.js", "/links.html", "/forms.html", "/classic.html", "/events.html", "/events.js");
     private final Socket socket;
 
-    public Handler(Socket socket)  {
+    public Handler(Socket socket) {
         this.socket = socket;
     }
 
@@ -34,7 +39,10 @@ public class Handler implements Runnable {
                 return;
             }
 
-            final var path = parts[1];
+            final var fullPath = parts[1];
+            final var path = fullPath.split("\\?")[0];
+            final Map<String, String> queryParams = getQueryParams(fullPath);
+
             if (!validPaths.contains(path)) {
                 out.write((
                         "HTTP/1.1 404 Not Found\r\n" +
@@ -51,11 +59,15 @@ public class Handler implements Runnable {
 
             // special case for classic
             if (path.equals("/classic.html")) {
-                final var template = Files.readString(filePath);
-                final var content = template.replace(
+                var template = Files.readString(filePath);
+                template = template.replace(
                         "{time}",
-                        LocalDateTime.now().toString()
-                ).getBytes();
+                        LocalDateTime.now().toString());
+                template = template.replace(
+                        "{user}",
+                        queryParams.get("name"));
+                var content = template.getBytes();
+
                 out.write((
                         "HTTP/1.1 200 OK\r\n" +
                                 "Content-Type: " + mimeType + "\r\n" +
@@ -63,6 +75,7 @@ public class Handler implements Runnable {
                                 "Connection: close\r\n" +
                                 "\r\n"
                 ).getBytes());
+
                 out.write(content);
                 out.flush();
                 return;
@@ -82,5 +95,15 @@ public class Handler implements Runnable {
         } catch (IOException exception) {
             exception.printStackTrace();
         }
+    }
+
+    private Map<String, String> getQueryParams(String path) {
+        Map<String, String> queryParams = new HashMap<>();
+        String paramsUrl = path.substring(path.indexOf("?")).substring(1);
+        URLEncodedUtils
+                .parse(paramsUrl, StandardCharsets.UTF_8)
+                .forEach(item -> queryParams.put(item.getName(), item.getValue()));
+
+        return queryParams;
     }
 }
